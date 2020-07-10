f5f4fefcbef2767e0bf26e7452c01028e99094e7
Nafisa Bulsara <nafisabulsara@Nafisas-MacBook-Pro.local>
2020-07-09 20:54:27 -0700

    removed test code

--- test.py
+++
@@ -1,9 +0,0 @@
-from markdown import markdown
-import pdfkit
-input_filename = 'FDA_Submission.md'
-output_filename = 'FDA_Submission.pdf'
-
-with open(input_filename, 'r') as f:
-    html_text = markdown(f.read(), output_format='html4')
-
-pdfkit.from_string(html_text, output_filename)
